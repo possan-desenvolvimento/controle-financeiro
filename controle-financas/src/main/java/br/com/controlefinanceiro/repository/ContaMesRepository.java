c47897f5a8fd6733b4f70eaecacd7c18c43350b2
1992-leonardo <leonardopossamdasilva@gmail.com>
2025-06-13 10:23:53 -0300

    Subindo meu projeto.

--- controle-financas/src/main/java/br/com/controlefinanceiro/repository/ContaMesRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.controlefinanceiro.repository;
-
-import br.com.controlefinanceiro.model.ContaMes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContaMesRepository extends JpaRepository<ContaMes, Integer> {
-}
